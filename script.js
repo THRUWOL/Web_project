ec33d22c4ac9356a72bedff085de970e48215884
Nikita Yarosh <yarosh.nv@yandex.ru>
2021-11-10 15:19:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+const screens = document.querySelectorAll(".screen");
+const chooseBirdButtons = document.querySelectorAll(".choose-bird-btn");
+const startButton = document.getElementById("start-btn");
+const gameContainer = document.getElementById("game-container");
+const timeElement = document.getElementById("time");
+const scoreElement = document.getElementById("score");
+const message = document.getElementById("message");
+let seconds = 0;
+let score = 0;
+let selectedBird = {};
+
+startButton.addEventListener("click", () => screens[0].classList.add("up"));
+
+const increaseScore = () => {
+  score++;
+  if (score > 100) message.classList.add("visible");
+  scoreElement.innerHTML = `Очки: ${score}`;
+};
+
+const addBirds = () => {
+  setTimeout(createBird, 1000);
+  setTimeout(createBird, 5000);
+};
+
+const catchBird = function () {
+  increaseScore();
+  this.classList.add("caught");
+  setTimeout(() => this.remove, 2000);
+  addBirds();
+};
+
+const getRandomLocation = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const x = Math.random() * (width - 200) + 100;
+  const y = Math.random() * (height - 200) + 100;
+  return { x, y };
+};
+
+function random() {
+  var randoms = Math.floor(Math.random() * 2);
+  if (randoms) return -1;
+  else return 1;
+}
+
+const createBird = () => {
+  const bird = document.createElement("div");
+  bird.classList.add("bird");
+  const { x, y } = getRandomLocation();
+  bird.style.top = `${y}px`;
+  bird.style.left = `${x}px`;
+  bird.innerHTML = `<img src="${selectedBird.src}" 
+  alt="${selectedBird.alt}"
+  style="transform: scale(${random()},1);"
+   />`;
+  bird.style.animation = `img-animation-${Math.floor(Math.random()*2)} ${Math.random() *10+10}s infinite`;
+  
+  bird.addEventListener("click", catchBird);
+  gameContainer.appendChild(bird);
+};
+
+const increaseTime = () => {
+  let m = Math.floor(seconds / 60);
+  let s = seconds % 60;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+  timeElement.innerHTML = `Время игры: ${m}:${s}`;
+  seconds++;
+};
+
+const startGame = () => setInterval(increaseTime, 1000);
+
+chooseBirdButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const image = button.querySelector("img");
+    const src = image.getAttribute("src");
+    selectedBird = { src};
+    screens[1].classList.add("up");
+    setTimeout(createBird, 1000);
+    startGame();
+  });
+});
